9c387751e82c2ed445e6f22a84bc044e0a90f929
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-05-09 20:21:28 -0400

    First Commit

---
+++ ActivityDashboard.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Octopus
+{
+    /// <summary>
+    /// Interaction logic for ActivityDashboard.xaml
+    /// </summary>
+    public partial class ActivityDashboard : Page
+    {
+        public ActivityDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void View_Activity_Click(object sender, RoutedEventArgs e)
+        {
+            ActivityView expenseReportPage = new ActivityView(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ ActivityView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Octopus
+{
+    /// <summary>
+    /// Interaction logic for ActivityView.xaml
+    /// </summary>
+    public partial class ActivityView : Page
+    {
+        public ActivityView()
+        {
+            InitializeComponent();
+        }
+
+        public ActivityView(object data) : this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+    }
+}
